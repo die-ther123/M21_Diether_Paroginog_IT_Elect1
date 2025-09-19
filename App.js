dd953ee95accd74ec162f5c1e7488abe5f952ef5
die-ther123 <paroginogdiether@gmail.com>
2025-09-19 14:48:53 +0800

    add counter and color changer

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import CounterApp from './CounterApp'
+import ColorChanger from './ColorChanger'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+<CounterApp/>
+<ColorChanger/>
       <StatusBar style="auto" />
     </View>
   );
